dc75ee76b2847c4de0fa86f716e1f5dd54387c89
Tomoyuki ikeda <rebirth1118@msn.com>
2024-12-10 19:37:40 +0900

    6章まで完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -53,6 +53,18 @@ export const getNewsList = async (queries?: MicroCMSQueries) => {
   return listData;
 };
 
+export const getNewsDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  const detailData = await client.getListDetail<News>({
+    endpoint: "news",
+    contentId,
+    queries,
+  });
+  return detailData;
+};
+
 type Props = {
   news: News[];
 };
