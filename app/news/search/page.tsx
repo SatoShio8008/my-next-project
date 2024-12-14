a99b86dfe59f5cef79e79544becc581ff20a260e
Tomoyuki ikeda <rebirth1118@msn.com>
2024-12-14 22:05:50 +0900

    7章まで完了

---
+++ app/news/search/page.tsx
@@ -0,0 +1,23 @@
+import { getNewsList } from "@/app/_libs/microcms";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import NewsList from "@/app/_components/NewsList";
+import SearchField from "@/app/_components/SearchField";
+
+type Props = {
+  searchParams: {
+    q?: string;
+  };
+};
+
+export default async function Page({ searchParams }: Props) {
+  const { contents: news } = await getNewsList({
+    q: searchParams.q,
+  });
+
+  return (
+    <>
+      <SearchField />
+      <NewsList news={news} />
+    </>
+  );
+}
